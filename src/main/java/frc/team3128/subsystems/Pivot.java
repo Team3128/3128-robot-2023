957957fb180891bbf24646eb6f705084e7137d5f
mika-okamoto <mika.disneypins@gmail.com>
2023-02-17 20:52:45 -0800

    pivot abs enc used in pid

--- src/main/java/frc/team3128/subsystems/Pivot.java
+++ src/main/java/frc/team3128/subsystems/Pivot.java
@@ -12,9 +12,12 @@ import frc.team3128.Constants.PivotConstants;
 import frc.team3128.common.hardware.motorcontroller.NAR_CANSparkMax;
 import frc.team3128.common.utility.NAR_Shuffleboard;
 
+import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import static frc.team3128.common.swerve.CTREConfigs.*;
+
 
 public class Pivot extends PIDSubsystem {
 
@@ -23,9 +26,12 @@ public class Pivot extends PIDSubsystem {
     private static Pivot instance;
     private NAR_CANSparkMax m_rotateMotor;
     private SparkMaxRelativeEncoder m_encoder;
+    private CANCoder m_cancoder;
 
     public Pivot() {
         super(new PIDController(kP, kI, kD));
+
+        getController().enableContinuousInput(-180, 180);
 
         configMotors();
         configEncoders();
@@ -51,6 +57,10 @@ public class Pivot extends PIDSubsystem {
     private void configEncoders() {
         m_encoder = (SparkMaxRelativeEncoder) m_rotateMotor.getEncoder();
         m_encoder.setPositionConversionFactor(ENC_CONV);
+        m_cancoder = new CANCoder(CANCODER_ID, "rio");
+        m_cancoder.configFactoryDefault();
+        m_cancoder.configAllSettings(swerveCancoderConfig());
+        // resetToAbsolute();
     }
 
     public void setPower(double power) {
@@ -62,12 +72,21 @@ public class Pivot extends PIDSubsystem {
         startPID(0);
     }
 
+    public double getCanCoder(){
+        return m_cancoder.getAbsolutePosition() - ANGLE_OFFSET;
+    }
+
+    public void resetToAbsolute() {
+        m_encoder.setPosition(getCanCoder()); // might reset the thing with weird gear ratios
+    }
+
     public void zeroEncoder() {
         m_encoder.setPosition(0);
     }
 
     public void initShuffleboard() {
         NAR_Shuffleboard.addData("pivot","pivot angle", ()->getMeasurement(),0,0);
+        NAR_Shuffleboard.addData("pivot","cancoder angle", ()->getCanCoder(),0,5);
         NAR_Shuffleboard.addData("pivot", "pivot setpoint", ()->getSetpoint(), 0, 1);
         kF = NAR_Shuffleboard.debug("pivot","kF", PivotConstants.kF, 0,2);
         setpoint = NAR_Shuffleboard.debug("pivot", "setpoint", 0, 1,2);
@@ -92,7 +111,8 @@ public class Pivot extends PIDSubsystem {
 
     @Override
     protected double getMeasurement() { // returns degrees
-       return m_rotateMotor.getSelectedSensorPosition() + MIN_ANGLE;
+        return getCanCoder() + MIN_ANGLE;
+    //    return m_rotateMotor.getSelectedSensorPosition() + MIN_ANGLE;
     }
 
     public void stopPivot() {
