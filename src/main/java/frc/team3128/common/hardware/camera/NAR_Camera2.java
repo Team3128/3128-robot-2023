06fc9b3b5fd2469ecee335ec3828d8234798f0f3
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-10-06 18:23:05 -0700

    more vision clean up

--- src/main/java/frc/team3128/common/hardware/camera/NAR_Camera2.java
+++ src/main/java/frc/team3128/common/hardware/camera/NAR_Camera2.java
@@ -37,12 +37,12 @@ public class NAR_Camera2 extends PhotonCamera {
     private PhotonTrackedTarget bestTarget;
 
     private static DoubleSupplier gyro;
-    
-    private static BiConsumer<Pose2d,Double> updatePose;
+
+    private static BiConsumer<Pose2d, Double> updatePose;
 
     private static HashMap<Integer, Pose2d> AprilTags;
     private static Pose2d visionTarget;
-    public static boolean multipleTargets = false;
+    public static boolean multipleTargets;
 
     public static DoubleSupplier thresh;
 
@@ -53,27 +53,12 @@ public class NAR_Camera2 extends PhotonCamera {
         setVersionCheckEnabled(false);
     }
 
-    public static void setRequirements(DoubleSupplier angle, BiConsumer<Pose2d,Double> odometry, HashMap<Integer,Pose2d> poses, boolean haveMultipleTargets){
+    public static void setRequirements(DoubleSupplier angle, BiConsumer<Pose2d, Double> odometry,
+            HashMap<Integer, Pose2d> poses, boolean haveMultipleTargets) {
         gyro = angle;
         updatePose = odometry;
         AprilTags = poses;
         multipleTargets = haveMultipleTargets;
-    }
-
-    public static void setGyro(DoubleSupplier angle) {
-        gyro = angle;
-    }
-
-    public static void setOdometry(BiConsumer<Pose2d,Double> odometry) {
-        updatePose = odometry;
-    }
-
-    public static void setAprilTags(HashMap<Integer, Pose2d> poses) {
-        AprilTags = poses;
-    }
-
-    public static void setVisionTarget(Pose2d pose) {
-        visionTarget = pose;
     }
 
     public void enable() {
@@ -89,19 +74,20 @@ public class NAR_Camera2 extends PhotonCamera {
         if (result.hasTargets()) {
             targets = result.getTargets();
             bestTarget = result.getBestTarget();
-            if (!camera.updatePose) return;
+            if (!camera.updatePose)
+                return;
             ArrayList<Pose2d> poses = new ArrayList<Pose2d>();
             if (multipleTargets) {
-                for (int i = 0; i < targets.size(); i ++) {
-                    if (targetAmbiguity(targets.get(i)) < 0.3 && !getPos(targets.get(i)).equals(new Pose2d()))
-                        poses.add(getPos(targets.get(i)));
+                for (int i = 0; i < targets.size(); i++) {
+                    if (targetAmbiguity(targets.get(i)) < 0.3 && !getPosApril(targets.get(i)).equals(new Pose2d()))
+                        poses.add(getPosApril(targets.get(i)));
                 }
-            }
-            else if (!getPos().equals(new Pose2d()))poses.add(getPos());
+            } else if (!getPosApril().equals(new Pose2d()))
+                poses.add(getPosApril());
             for (int i = 0; i < poses.size(); i++) {
                 if (translationOutOfBounds(poses.get(i).getTranslation()))
                     return;
-                updatePose.accept(poses.get(i),result.getTimestampSeconds());
+                updatePose.accept(poses.get(i), result.getTimestampSeconds());
             }
             return;
         }
@@ -130,20 +116,13 @@ public class NAR_Camera2 extends PhotonCamera {
         return hasValidTarget() ? target.getPitch() : 0;
     }
 
+    // TODO: remove?
     public double targetArea() {
         return targetArea(bestTarget);
     }
 
     private double targetArea(PhotonTrackedTarget target) {
         return hasValidTarget() ? target.getArea() : 0;
-    }
-
-    public double targetSkew() {
-        return targetSkew(bestTarget);
-    }
-
-    private double targetSkew(PhotonTrackedTarget target) {
-        return hasValidTarget() ? target.getSkew() : 0;
     }
 
     public int targetId() {
@@ -173,64 +152,50 @@ public class NAR_Camera2 extends PhotonCamera {
     public Transform2d getTarget() {
         return getTarget(bestTarget);
     }
-    
+
+    // rawTarget() + transform
     private Transform2d getTarget(PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return new Transform2d();
+        if (!hasValidTarget())
+            return new Transform2d();
         Transform3d transform = getRawTarget(target);
-        return new Transform2d(transform.getTranslation().toTranslation2d(), transform.getRotation().toRotation2d().unaryMinus());
+        return new Transform2d(transform.getTranslation().toTranslation2d(),
+                transform.getRotation().toRotation2d().unaryMinus());
     }
 
     public Transform2d getProcessedTarget() {
         return getProcessedTarget(bestTarget);
     }
 
+    // process getTarget()
     private Transform2d getProcessedTarget(PhotonTrackedTarget target) {
-        if (!hasValidTarget() || !AprilTags.containsKey(targetId(target))) return new Transform2d();
+        if (!hasValidTarget() || !AprilTags.containsKey(targetId(target)))
+            return new Transform2d();
         double hypotenuse = getAprilDistance(target);
         Rotation2d angle = getTarget().getRotation();
         double targetAngle = AprilTags.get(targetId(target)).getRotation().getDegrees();
-        double deltaY = hypotenuse * Math.sin(Units.degreesToRadians(gyro.getAsDouble() + targetAngle + camera.offset.getRotation().getDegrees()));
+        double deltaY = hypotenuse * Math.sin(
+                Units.degreesToRadians(gyro.getAsDouble() + targetAngle + camera.offset.getRotation().getDegrees()));
         Transform2d vector = getTarget(target);
         return new Transform2d(new Translation2d(vector.getX(), vector.getY() - deltaY), angle);
     }
-
-    public Transform2d getTest() {
-        return getTest(bestTarget);
-    }
-
-    private Transform2d getTest(PhotonTrackedTarget target) {
-        if (!hasValidTarget() || !AprilTags.containsKey(targetId(target))) return new Transform2d();
-        Rotation2d angle = getTarget().getRotation();
-        double targetAngle = AprilTags.get(targetId(target)).getRotation().getDegrees();
-        Transform2d vector = getTarget(target);
-        double deltaY = vector.getX() * Math.tan(Units.degreesToRadians(gyro.getAsDouble() + targetAngle));
-        return new Transform2d(new Translation2d(vector.getX(), vector.getY() + deltaY), angle);
-    }
-    
 
     public boolean hasValidTarget() {
         return targets != null;
     }
 
-    public double getDistance() {
+    public double getAprilDistance() {
         return getAprilDistance(bestTarget);
     }
 
     // Relative to Camera
     private double getAprilDistance(PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return -1;
+        if (!hasValidTarget())
+            return -1;
         Transform2d transform = getTarget(target);
         return Math.sqrt(Math.pow(transform.getX(), 2) + Math.pow(transform.getY(), 2));
     }
 
-    // Relative to Camera
-    private double getVisionDistance(PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return -1;
-        double ty = Units.degreesToRadians(targetPitch(target) + camera.angle);
-        double tx = Units.degreesToRadians(targetYaw(target));
-        return Math.abs(camera.targetHeight - camera.height) / (Math.tan(ty) * Math.cos(tx));
-    }
-
+    // TODO: remove?
     public void setLED(boolean state) {
         setLED(state ? VisionLEDMode.kOn : VisionLEDMode.kOff);
     }
@@ -240,89 +205,37 @@ public class NAR_Camera2 extends PhotonCamera {
     }
 
     private Pose2d getTargetPosApril(Pose2d robotPos, PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return new Pose2d();
+        if (!hasValidTarget())
+            return new Pose2d();
         Pose2d cameraPos = robotPos.transformBy(camera.offset.inverse());
         Transform2d transform = getTarget(target).inverse();
         return cameraPos.plus(transform);
     }
 
-    private Pose2d getTargetPosVision(Pose2d robotPos, PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return new Pose2d();
-        Pose2d cameraPos = robotPos.transformBy(camera.offset.inverse());
-        Double distance = getVisionDistance(target);
-        Double angle = cameraPos.getRotation().getRadians() - Units.degreesToRadians(targetYaw(target));
-        return new Pose2d(cameraPos.getX() + distance * Math.cos(angle), cameraPos.getY() + distance * Math.sin(angle),
-                new Rotation2d(angle));
-    }
-
-    public Pose2d getPos() {
-        return getPos(bestTarget);
-    }
-
-    private Pose2d getPos(PhotonTrackedTarget target) {
-        return getPosApril(target);
+    public Pose2d getPosApril() {
+        return getPosApril(bestTarget);
     }
 
     // Relative to Robot
     private Pose2d getPosApril(PhotonTrackedTarget tag) {
-        if(!hasValidTarget() || !AprilTags.containsKey(targetId(tag))) return new Pose2d();
+        if (!hasValidTarget() || !AprilTags.containsKey(targetId(tag)))
+            return new Pose2d();
         Transform2d transform = getProcessedTarget(tag);
-        if (!AprilTags.containsKey(targetId(tag)) || transform.getX() > 5 || Math.abs(transform.getRotation().getDegrees()) < 150) return new Pose2d();
+        if (!AprilTags.containsKey(targetId(tag)) || transform.getX() > 5
+                || Math.abs(transform.getRotation().getDegrees()) < 150)
+            return new Pose2d();
         Pose2d target = AprilTags.get(targetId());
-        if (target == null) return new Pose2d();
+        if (target == null)
+            return new Pose2d();
         Translation2d coord = target.getTranslation().plus(transform.getTranslation().rotateBy(target.getRotation()));
         Rotation2d angle = target.getRotation().plus(transform.getRotation());
 
-        Pose2d pos = new Pose2d(coord,angle);
+        Pose2d pos = new Pose2d(coord, angle);
 
         return pos.transformBy(camera.offset);
     }
 
-    // Relative to Robot
-    private Pose2d getPosVision(PhotonTrackedTarget target) {
-        if (!hasValidTarget()) return new Pose2d();
-        double distance = getVisionDistance(target);
-        double yaw = Units.degreesToRadians(targetYaw(target));
-        Translation2d translation = new Translation2d(distance * Math.cos(yaw), distance * Math.sin(yaw));
-
-        Transform2d transform = new Transform2d(translation,
-                new Rotation2d(Units.degreesToRadians(gyro.getAsDouble())));
-        Pose2d pos = visionTarget.transformBy(transform.inverse());
-        return pos.transformBy(camera.offset);
-    }
-
-    // Relative to Robot
-    @Deprecated
-    private Pose2d visionEstimatedPose(Pose2d pose) {
-        if (!hasValidTarget())
-            return new Pose2d();
-        double distToHubCenter = getDistance();
-        Rotation2d thetaHub = Rotation2d.fromDegrees(gyro.getAsDouble() - targetYaw());
-        Translation2d fieldPos = new Translation2d(-distToHubCenter * Math.cos(thetaHub.getRadians()),
-                -distToHubCenter * Math.sin(thetaHub.getRadians()))
-                .plus(pose.getTranslation());
-        Pose2d pos = new Pose2d(fieldPos, Rotation2d.fromDegrees(gyro.getAsDouble()));
-        pos.transformBy(camera.offset);
-
-        return pos;
-    }
-
-    // Relative to Robot
-    @Deprecated
-    private Pose2d visionPos(Pose2d pose) {
-        if (!hasValidTarget())
-            return new Pose2d();
-        double distToHubCenter = getDistance();
-        double angle = Units.degreesToRadians(gyro.getAsDouble() - targetYaw() - 180);
-        Translation2d fieldPos = new Translation2d(distToHubCenter * Math.cos(angle), distToHubCenter * Math.sin(angle))
-                .plus(pose.getTranslation());
-        Pose2d pos = new Pose2d(fieldPos, Rotation2d.fromDegrees(gyro.getAsDouble()));
-        pos.transformBy(camera.offset);
-
-        return pos;
-    } 
-
-    public String get_name() {
+    public String getName() {
         return camera.hostname;
     }
 
--- src/main/java/frc/team3128/subsystems/Vision.java
+++ src/main/java/frc/team3128/subsystems/Vision.java
@@ -14,18 +14,18 @@ import frc.team3128.common.hardware.camera.NAR_Camera;
 import frc.team3128.common.hardware.camera.NAR_Camera2;
 import frc.team3128.common.utility.NAR_Shuffleboard;
 
-public class Vision extends SubsystemBase{
+public class Vision extends SubsystemBase {
     public static int SELECTED_GRID = 0;
     public static boolean AUTO_ENABLED = false;
     public static boolean MANUAL = false;
     public static ArmPosition position = ArmPosition.NEUTRAL;
 
-    private HashMap<String,NAR_Camera> cameras;
-  
+    private HashMap<String, NAR_Camera> cameras;
+
     private static Vision instance;
 
-    public static synchronized Vision getInstance(){
-        if(instance == null) {
+    public static synchronized Vision getInstance() {
+        if (instance == null) {
             instance = new Vision();
         }
         return instance;
@@ -33,8 +33,8 @@ public class Vision extends SubsystemBase{
 
     public Vision() {
         Swerve swerve = Swerve.getInstance();
-        NAR_Camera2.setRequirements(()-> swerve.getYaw(), (pose,time) -> swerve.addVisionMeasurement(pose,time), APRIL_TAG_POS, false);
-        cameras = new HashMap<String,NAR_Camera>();
+        NAR_Camera2.setRequirements(() -> swerve.getYaw(), (pose, time) -> swerve.addVisionMeasurement(pose, time), APRIL_TAG_POS, false);
+        cameras = new HashMap<String, NAR_Camera>();
         cameras.put(FRONT.hostname, new NAR_Camera(FRONT));
         cameras.put(BACK.hostname, new NAR_Camera(BACK));
     }
@@ -48,7 +48,7 @@ public class Vision extends SubsystemBase{
         for (NAR_Camera cam : getCameras()) {
             Pose2d pose = cam.getPos();
             if (!pose.equals(new Pose2d())) {
-                swerve.resetOdometry(new Pose2d(pose.getTranslation(),swerve.getGyroRotation2d()));
+                swerve.resetOdometry(new Pose2d(pose.getTranslation(), swerve.getGyroRotation2d()));
                 return;
             }
         }
@@ -111,51 +111,56 @@ public class Vision extends SubsystemBase{
         return cameras.get(name).camera;
     }
 
-    public Collection<NAR_Camera> getCameras(){
+    public Collection<NAR_Camera> getCameras() {
         return cameras.values();
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
         for (NAR_Camera cam : getCameras()) {
             cam.update();
         }
     }
-    
+
     public void initShuffleboard() {
         NAR_Camera cam = cameras.get(FRONT.hostname);
         NAR_Camera cam2 = cameras.get(BACK.hostname);
-        NAR_Shuffleboard.addData("VisionComp", "HasTarget", ()->cam.hasValidTarget() || cam2.hasValidTarget(), 0, 0);
+        NAR_Shuffleboard.addData("VisionComp", "HasTarget", () -> cam.hasValidTarget() || cam2.hasValidTarget(), 0, 0);
 
-        NAR_Shuffleboard.addData("Vision","HasTarget", ()->cam.hasValidTarget(), 0, 0);
-        NAR_Shuffleboard.addData("Vision","Distance",()->cam.getDistance(),1,0);
-        NAR_Shuffleboard.addData("Vision","RawTarget",()->cam.getTarget().toString(),0,1,4,1);
-        NAR_Shuffleboard.addData("Vision", "Processed Target",()->cam.getProcessedTarget().toString(),0,2,4,1);
-        NAR_Shuffleboard.addData("Vision","EstimatedPose", ()-> cam.getPos().toString(),0,3,4,1);
-        NAR_Shuffleboard.addData("Drivetrain", "HasTarget", ()-> cam.hasValidTarget(), 1, 1);
-        NAR_Shuffleboard.addData("Test", "Test", ()->SELECTED_GRID,0,0);
-        NAR_Shuffleboard.addData("Test", "TESTING", ()->cam.getTest().toString(),0,1,3,1);
+        NAR_Shuffleboard.addData("Vision", "HasTarget", () -> cam.hasValidTarget(), 0, 0);
+        NAR_Shuffleboard.addData("Vision", "Distance", () -> cam.getDistance(), 1, 0);
+        NAR_Shuffleboard.addData("Vision", "RawTarget", () -> cam.getTarget().toString(), 0, 1, 4, 1);
+        NAR_Shuffleboard.addData("Vision", "Processed Target", () -> cam.getProcessedTarget().toString(), 0, 2, 4, 1);
+        NAR_Shuffleboard.addData("Vision", "EstimatedPose", () -> cam.getPos().toString(), 0, 3, 4, 1);
+        NAR_Shuffleboard.addData("Drivetrain", "HasTarget", () -> cam.hasValidTarget(), 1, 1);
+        NAR_Shuffleboard.addData("Test", "Test", () -> SELECTED_GRID, 0, 0);
+        NAR_Shuffleboard.addData("Test", "TESTING", () -> cam.getTest().toString(), 0, 1, 3, 1);
 
-        NAR_Shuffleboard.addData("Vision2","HasTarget", ()->cam2.hasValidTarget(), 0, 0);
-        NAR_Shuffleboard.addData("Vision2","Distance",()->cam2.getDistance(),1,0);
-        NAR_Shuffleboard.addData("Vision2","RawTarget",()->cam2.getTarget().toString(),0,1,4,1);
-        NAR_Shuffleboard.addData("Vision2", "Processed Target",()->cam2.getProcessedTarget().toString(),0,2,4,1);
-        NAR_Shuffleboard.addData("Vision2","EstimatedPose", ()-> cam2.getPos().toString(),0,3,4,1);
-        NAR_Shuffleboard.addData("Test", "TESTING", ()->cam2.getTest().toString(),0,2,3,1);
+        NAR_Shuffleboard.addData("Vision2", "HasTarget", () -> cam2.hasValidTarget(), 0, 0);
+        NAR_Shuffleboard.addData("Vision2", "Distance", () -> cam2.getDistance(), 1, 0);
+        NAR_Shuffleboard.addData("Vision2", "RawTarget", () -> cam2.getTarget().toString(), 0, 1, 4, 1);
+        NAR_Shuffleboard.addData("Vision2", "Processed Target", () -> cam2.getProcessedTarget().toString(), 0, 2, 4, 1);
+        NAR_Shuffleboard.addData("Vision2", "EstimatedPose", () -> cam2.getPos().toString(), 0, 3, 4, 1);
+        NAR_Shuffleboard.addData("Test", "TESTING", () -> cam2.getTest().toString(), 0, 2, 3, 1);
 
-        NAR_Shuffleboard.addData("Togglables", "AUTO_ENABLED", ()-> AUTO_ENABLED, 0, 0);
-        NAR_Shuffleboard.addData("Togglables", "MANUAL", ()-> MANUAL, 1, 0);
+        NAR_Shuffleboard.addData("Togglables", "AUTO_ENABLED", () -> AUTO_ENABLED, 0, 0);
+        NAR_Shuffleboard.addData("Togglables", "MANUAL", () -> MANUAL, 1, 0);
     }
 
     public void logCameraAll() {
-        NAR_Shuffleboard.addData("Vision Urgent", "HasTarget", cameras.get(FRONT.hostname).hasValidTarget(),0,0);
-        NAR_Shuffleboard.addData("Vision Urgent", "Distance", calculateDistance(FRONT.hostname),1,0);
-        NAR_Shuffleboard.addData("Vision Urgent", "EstimatedPose",cameras.get(FRONT.hostname).getPos().toString(),0,1);
-        NAR_Shuffleboard.addData("Vision Urgent", "GEICO",cameras.get(FRONT.hostname).targetYaw(),2,0);
-        NAR_Shuffleboard.addData("Vision Urgent", "TARGETPOS",cameras.get(FRONT.hostname).getTargetPos(Swerve.getInstance().getPose()).toString(),0,2);
-        NAR_Shuffleboard.addData("Vision Urgent", "RAWTARGET",cameras.get(FRONT.hostname).getTarget().toString(),0,3);
-        NAR_Shuffleboard.addData("Vision Urgent", "TARGETGUITY",cameras.get(FRONT.hostname).targetAmbiguity(),3,0);
-        NAR_Shuffleboard.addData("Vision Urgent", "PROCESSED TARGET", cameras.get(FRONT.hostname).getProcessedTarget().toString(),0,4);
-        NAR_Shuffleboard.addData("Vision Urgent", "SUNSHINE", cameras.get(FRONT.hostname).getRawTarget().toString(),0,5);
+        NAR_Shuffleboard.addData("Vision Urgent", "HasTarget", cameras.get(FRONT.hostname).hasValidTarget(), 0, 0);
+        NAR_Shuffleboard.addData("Vision Urgent", "Distance", calculateDistance(FRONT.hostname), 1, 0);
+        NAR_Shuffleboard.addData("Vision Urgent", "EstimatedPose", cameras.get(FRONT.hostname).getPos().toString(), 0,
+                1);
+        NAR_Shuffleboard.addData("Vision Urgent", "GEICO", cameras.get(FRONT.hostname).targetYaw(), 2, 0);
+        NAR_Shuffleboard.addData("Vision Urgent", "TARGETPOS",
+                cameras.get(FRONT.hostname).getTargetPos(Swerve.getInstance().getPose()).toString(), 0, 2);
+        NAR_Shuffleboard.addData("Vision Urgent", "RAWTARGET", cameras.get(FRONT.hostname).getTarget().toString(), 0,
+                3);
+        NAR_Shuffleboard.addData("Vision Urgent", "TARGETGUITY", cameras.get(FRONT.hostname).targetAmbiguity(), 3, 0);
+        NAR_Shuffleboard.addData("Vision Urgent", "PROCESSED TARGET",
+                cameras.get(FRONT.hostname).getProcessedTarget().toString(), 0, 4);
+        NAR_Shuffleboard.addData("Vision Urgent", "SUNSHINE", cameras.get(FRONT.hostname).getRawTarget().toString(), 0,
+                5);
     }
 }
